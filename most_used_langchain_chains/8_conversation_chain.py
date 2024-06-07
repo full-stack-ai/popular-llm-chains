ec5b5209d4b2d7b5a972a73466d9afde7fd4c8fd
full-stack-ai <fullstackinfo.ai@gmail.com>
2024-06-07 15:18:28 -0600

    update conversation chain

--- most_used_langchain_chains/8_conversation_chain.py
+++ most_used_langchain_chains/8_conversation_chain.py
@@ -1,7 +1,6 @@
 from langchain.chains.conversation.base import ConversationChain
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory, ConversationBufferWindowMemory, ConversationKGMemory
 from langchain_core.prompts.prompt import PromptTemplate
-from langchain.chains.llm import LLMChain
 from langchain_openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
@@ -18,11 +17,19 @@ template = """
 """
 prompt = PromptTemplate(input_variables=["history", "input"], template=template)
 
+memory = ConversationBufferMemory(ai_prefix="AI Assistant", user_prefix="Friend")
+# memory = ConversationSummaryMemory(ai_prefix="AI Assistant", user_prefix="Friend")
+# memory = ConversationBufferWindowMemory(ai_prefix="AI Assistant", user_prefix="Friend", k=4, memory_key="chat_history")
+
 conversation = ConversationChain(
     prompt=prompt,
     llm=llm,
     verbose=True,
-    memory=ConversationBufferMemory(ai_prefix="AI Assistant", user_prefix="Friend"),
+    memory=memory,
 )
 conversation.predict(input="Hi There!")
-print(conversation.predict(input="What's the weather like today in Edmonton?"))
+conversation.predict(input="Who will win the Stanley Cup in 2024? Oilers or Panthers?")
+conversation.predict(input="I want to know how self-attention impacts the performance of transformer models.")
+conversation.predict(input="Are there any optimization techniques for these models?")
+conversation.predict(input="Can you write a c code to develop a transformer model?")
+print(conversation.predict(input="Do you know how I started my conversation with you?"))
